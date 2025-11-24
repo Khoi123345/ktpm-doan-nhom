33ba7fbc2b6e02e86fa9413dbc93d72623bf46a1
Duy Huy <147736742+Hidrose@users.noreply.github.com>
2025-11-24 21:55:49 +0700

    Remove all current folders

--- bookstore_frontend/src/App.js
+++
@@ -1,8 +0,0 @@
-import BasicRoute from "./Router";
-function App() {
-  return (
-    <BasicRoute/>
-  );
-}
-
-export default App;
--- bookstore_frontend/src/services/orderService.js
+++
@@ -1,7 +0,0 @@
-import config from '../config.json'
-import {postRequest, postRequest_v2} from "../utils/ajax";
-
-export const getOrder=(callback)=>{
-    const url = `${config.apiUrl}/getOrder`;
-    postRequest(url, {}, callback);
-};
