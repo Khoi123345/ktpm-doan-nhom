f27daca30a8dac91ae0a7db05c4c8dd32bae6b76
Khoi123345 <hoquockhoi.114@gmail.com>
2025-11-29 22:03:48 +0700

    fix: package-lock.json

--- frontend/vite.config.admin.js
+++ frontend/vite.config.admin.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
+    build: {
+        outDir: 'dist/admin',
+    },
     server: {
         port: 3001,
         proxy: {
--- frontend/vite.config.customer.js
+++ frontend/vite.config.customer.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
+    build: {
+        outDir: 'dist/customer',
+    },
     server: {
         port: 3000,
         proxy: {
